af9fca0f16436c50b3de5c7a9b8f20dbb20589ad
MH Lim <mhlim89@gmail.com>
2025-12-31 03:37:35 +0800

    Updated macro_alignof.h to prevent a build issue when the ALIGNOF macro is used in pre-C++11 source files for MSVC

--- includes/macro_alignof.h
+++ includes/macro_alignof.h
@@ -2,13 +2,23 @@
 #define MACRO_ALIGNOF_H
 
 #if defined( __cplusplus) && (__cplusplus >= 201103L)
+
 #define ALIGNOF(TYPE) alignof(TYPE)
+
 #elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 201112L)
+
 #include <stdalign.h>
 #define ALIGNOF(TYPE) alignof(TYPE)
+
 #else
+
+#if !(defined(_MSC_VER) && defined(__cplusplus))
 #include <stddef.h>
 #define ALIGNOF(TYPE) offsetof(struct {char ch; TYPE data;}, data)
+#else
+#define ALIGNOF(TYPE) __alignof(TYPE)
+#endif
+
 #endif
 
 #endif
